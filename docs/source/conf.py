aa3cf2d02549aa8ea198a24cde3e4ac9e024b327
Jie <dingj@umn.edu>
2024-09-08 00:17:47 -0500

    rm version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,9 +66,9 @@ html_js_files = [
 
 html_theme_options = {
     'canonical_url': '',
-    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'analytics_id': 'G-ZDQXZS0531',  #  Provided by Google in your dashboard
     'logo_only': False,
-    'display_version': True,
+    'display_version': False,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
